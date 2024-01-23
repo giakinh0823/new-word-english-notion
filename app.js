1105626f6da9976c04987f6e5b41af13e6d273c4
kinhhg1 <kinhhg1@ghtk.co>
2024-01-23 13:56:19 +0700

    update

--- app.js
+++ app.js
@@ -10,15 +10,15 @@ const port = 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// schedule.scheduleJob('*/10 * * * * *', async () => {
-//     console.log('Scheduled task running...');
-//     try {
-//         await fillValuesNewWords();
-//     } catch (error) {
-//         console.error('Error in scheduled task:', error);
-//     }
-//     console.log('Scheduled task done...');
-// });
+schedule.scheduleJob('*/10 * * * * *', async () => {
+    console.log('Scheduled task running...');
+    try {
+        await fillValuesNewWords();
+    } catch (error) {
+        console.error('Error in scheduled task:', error);
+    }
+    console.log('Scheduled task done...');
+});
 
 bot.launch();
 
